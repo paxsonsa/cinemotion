a4c2f72a254ce3274f125797ca6e88cf46b2291a
Andrew Paxson <13216842+paxsonsa@users.noreply.github.com>
2021-09-04 22:30:39 -0700

    add testing

---
+++ tests/indiemotion/test_server.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 Andrew Paxson. All rights reserved. Used under
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+/* server.hpp 
+
+*/
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+TEST_CASE("Test Example")
+{
+    CHECK_EQ(2, 2);
+}
+
+TEST_CASE("Test Another Example")
+{
+    CHECK_EQ(1, 1);
+}
