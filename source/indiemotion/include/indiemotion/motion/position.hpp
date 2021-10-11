cfdbadf9c779ac161f9835dba766c53046b3d940
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2021-10-10 17:54:11 -0700

    add pragma once

--- source/indiemotion/include/indiemotion/motion/position.hpp
+++ source/indiemotion/include/indiemotion/motion/position.hpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2021 Andrew Paxson. All rights reserved. Used under
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 /* position.hpp */
+#pragma once
 #include <indiemotion/_common.hpp>
 
 namespace indiemotion::motion
