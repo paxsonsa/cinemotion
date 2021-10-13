91ba25f919ed17a478a3e437cfb3b75c2ccd7cb1
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2021-10-12 20:54:17 -0700

    create new protobuf header in messages

---
+++ source/indiemotion/include/indiemotion/messages/protobuf.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2021 Andrew Paxson. All rights reserved. Used under
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+/* protobuf.hpp 
+Helper header to abstract the include of the indiemotion protocol bufs
+*/
+#pragma once
+#include <indiemotion-protobufs/messages.pb.h>
