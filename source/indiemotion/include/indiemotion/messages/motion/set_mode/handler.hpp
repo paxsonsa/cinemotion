9a8e9b74b2d29015eff81f4753d0bcf84e98c3c3
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2021-10-20 01:20:17 -0700

    fmt

--- source/indiemotion/include/indiemotion/messages/motion/set_mode/handler.hpp
+++ source/indiemotion/include/indiemotion/messages/motion/set_mode/handler.hpp
@@ -1,33 +1,31 @@
 // Copyright (c) 2021 Andrew Paxson. All rights reserved. Used under
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// Licensed under the MIT License. See LICENSE file in the project root for full
+// license information.
 /* handler.hpp */
 #pragma once
 #include <indiemotion/messages/base/handler.hpp>
 #include <indiemotion/messages/motion/set_mode/payload.hpp>
-#include <indiemotion/responses/acknowledge/payload.hpp>
 #include <indiemotion/responses.hpp>
 
-namespace indiemotion::messages::motion::setmode
-{
-    class Handler : public base::Handler
-    {
-    public:
-        std::optional<std::unique_ptr<responses::base::Response>>
-        handleMessage(std::weak_ptr<session::Session> sessionPtr,
-                      std::unique_ptr<base::Message> messagePtr)
-        {
-            auto payloadPtr = messagePtr->payloadPtrAs<Payload>();
-            auto mode = payloadPtr->newMode();
-            if (auto session = sessionPtr.lock())
-            {
-                session->updateMotionMode(mode);
-                auto payloadPtr = std::make_unique<responses::acknowledge::Payload>(true, "mode set.");
-                auto ctnPtr = responses::base::createResponse(messagePtr->header()->id(), std::move(payloadPtr));
-                return ctnPtr;
-            }
+namespace indiemotion::messages::motion::setmode {
+class Handler : public base::Handler {
+public:
+  std::optional<std::unique_ptr<responses::base::Response>>
+  handleMessage(std::weak_ptr<session::Session> sessionPtr,
+                std::unique_ptr<base::Message> messagePtr) {
+    auto payloadPtr = messagePtr->payloadPtrAs<Payload>();
+    auto mode = payloadPtr->newMode();
+    if (auto session = sessionPtr.lock()) {
+      session->updateMotionMode(mode);
+      auto payloadPtr =
+          std::make_unique<responses::acknowledge::Payload>(true, "mode set.");
+      auto ctnPtr = responses::base::createResponse(messagePtr->header()->id(),
+                                                    std::move(payloadPtr));
+      return ctnPtr;
+    }
 
-            // TODO Error (Session Gone)
-            return {};
-        }
-    };
-}
+    // TODO Error (Session Gone)
+    return {};
+  }
+};
+} // namespace indiemotion::messages::motion::setmode
