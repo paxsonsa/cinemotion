3c403a0e1a6f7778495ccdd702ffabc9e572cc19
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2021-11-01 03:27:21 -0700

    refactor xform data to allow moves

--- source/indiemotion/include/indiemotion/motion/xform.hpp
+++ source/indiemotion/include/indiemotion/motion/xform.hpp
@@ -1,17 +1,20 @@
 // Copyright (c) 2021 Andrew Paxson. All rights reserved. Used under
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
-/* position.hpp */
+/* xform.hpp */
 #pragma once
 #include <indiemotion/common.hpp>
 
 namespace indiemotion::motion
 {
-
     struct _XYZContainer
     {
-        double x;
-        double y;
-        double z;
+        double x = 0.0f;
+        double y = 0.0f;
+        double z = 0.0f;
+
+        _XYZContainer() {}
+        _XYZContainer(_XYZContainer &&mE) = default;
+        _XYZContainer &operator=(_XYZContainer &&mE) = default;
 
         static _XYZContainer zero()
         {
@@ -41,6 +44,28 @@ namespace indiemotion::motion
         MotionTranslation translation;
         MotionOrientation orientation;
 
+        MotionXForm()
+        {
+            translation = MotionTranslation::zero();
+            orientation = MotionOrientation::zero();
+        }
+
+        MotionXForm(MotionXForm &&rhs)
+        {
+            swap(std::move(rhs));
+        }
+        MotionXForm &operator=(MotionXForm &&rhs)
+        {
+            swap(std::move(rhs));
+            return *this;
+        }
+
+        void swap(MotionXForm &&rhs)
+        {
+            std::swap(translation, rhs.translation);
+            std::swap(orientation, rhs.orientation);
+        }
+
         static MotionXForm zero()
         {
             auto xform = MotionXForm();
@@ -50,7 +75,7 @@ namespace indiemotion::motion
         }
 
         static MotionXForm create(double tx, double ty, double tz,
-                                                   double ox, double oy, double oz)
+                                  double ox, double oy, double oz)
         {
             auto xform = MotionXForm();
             xform.translation = MotionTranslation::create(tx, ty, tz);
---
+++ tests/indiemotion/test_motion_xform.cpp
@@ -0,0 +1,37 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#include <indiemotion/common.hpp>
+#include <indiemotion/motion/xform.hpp>
+
+using namespace indiemotion;
+
+SCENARIO("creating motion xform")
+{
+    GIVEN("a xform")
+    {
+        auto xform = motion::MotionXForm::create(
+            1.0f, 2.0f, 3.0f,
+            4.0f, 5.0f, 6.0f);
+
+        WHEN("it is moved")
+        {
+            auto newXform = std::move(xform);
+            THEN("the object is the same")
+            {
+                REQUIRE(xform.translation.x == 0.0f);
+                REQUIRE(xform.translation.y == 0.0f);
+                REQUIRE(xform.translation.z == 0.0f);
+                REQUIRE(xform.orientation.x == 0.0f);
+                REQUIRE(xform.orientation.y == 0.0f);
+                REQUIRE(xform.orientation.z == 0.0f);
+
+                REQUIRE(newXform.translation.x == 1.0f);
+                REQUIRE(newXform.translation.y == 2.0f);
+                REQUIRE(newXform.translation.z == 3.0f);
+                REQUIRE(newXform.orientation.x == 4.0f);
+                REQUIRE(newXform.orientation.y == 5.0f);
+                REQUIRE(newXform.orientation.z == 6.0f);
+            }
+        }
+    }
+}
