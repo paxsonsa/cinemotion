69b180604f0618b3edf800e8842b0fb74712a45f
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2021-11-01 03:40:34 -0700

    add motion xform payload type

--- source/indiemotion/include/indiemotion/net/motion.hpp
+++ source/indiemotion/include/indiemotion/net/motion.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <indiemotion/common.hpp>
 #include <indiemotion/motion/mode.hpp>
+#include <indiemotion/motion/xform.hpp>
 
 namespace indiemotion::net
 {
@@ -13,6 +14,19 @@ namespace indiemotion::net
         PayloadType type() const
         {
             return PayloadType::SetMotionMode;
+        }
+    };
+
+    struct UpdateMotionXForm : public net::Payload_T
+    {
+        motion::MotionXForm xform;
+
+        UpdateMotionXForm(motion::MotionXForm xform) : xform(xform) {}
+        UpdateMotionXForm(motion::MotionXForm &&xform) : xform(xform) {}
+
+        PayloadType type() const
+        {
+            return PayloadType::UpdateMotionXForm;
         }
     };
 }
