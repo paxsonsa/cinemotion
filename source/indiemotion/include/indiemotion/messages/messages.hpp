b611babbcf6535e348e3963ad93645934b465ffb
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2021-11-01 19:08:49 -0700

    remove old structures

--- source/indiemotion/include/indiemotion/messages/messages.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021 Andrew Paxson. All rights reserved. Used under
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
-/* messages.hpp */
-#pragma once
-#include <indiemotion/messages/base/message.hpp>
-#include <indiemotion/messages/cameras/list/handler.hpp>
-#include <indiemotion/messages/cameras/list/payload.hpp>
-#include <indiemotion/messages/kind.hpp>
--- source/indiemotion/include/indiemotion/messages/motion/motion.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <indiemotion/messages/motion/get_mode.hpp>
-#include <indiemotion/messages/motion/set_mode.hpp>
-#include <indiemotion/messages/motion/xform.hpp>
