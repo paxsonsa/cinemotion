e92884e7a081ae915438a7cca19d3f3552dd2ccc
paxsonsa <13216842+paxsonsa@users.noreply.github.com>
2022-01-04 14:46:24 -0800

    append to integration testing

--- tests/indiemotion/integrations/test_integration_basic_run.cpp
+++ tests/indiemotion/integrations/test_integration_basic_run.cpp
@@ -9,9 +9,66 @@ using namespace indiemotion;
 
 struct DummyDelegate : SessionControllerDelegate
 {
+	DummyDelegate() {}
+
 	std::optional<Camera> get_camera_by_name(std::string name) override
 	{
 		return Camera(name);
+	}
+
+	bool get_available_cameras_called = false;
+	std::vector<Camera> get_available_cameras() override
+	{
+		get_available_cameras_called = true;
+		return {
+			Camera("cam1"),
+			Camera("cam2"),
+			Camera("cam3"),
+		};
+	}
+
+	std::optional<Camera> set_active_camera_value;
+	void did_set_active_camera(Camera camera) override
+	{
+		set_active_camera_value = camera;
+	}
+
+	MotionMode did_set_motion_mode_value;
+	void did_set_motion_mode(MotionMode m) override
+	{
+		did_set_motion_mode_value = m;
+	}
+
+	std::optional<MotionXForm> received_motion_xform;
+	void did_receive_motion_update(MotionXForm m) override
+	{
+		received_motion_xform = m;
+	}
+
+	bool will_shutdown_session_called = false;
+	void will_shutdown_session() override
+	{
+		will_shutdown_session_called = true;
+	}
+
+	bool will_start_session_called = false;
+	void will_start_session() override
+	{
+		will_start_session_called = true;
+		SessionControllerDelegate::will_start_session();
+	}
+
+	bool did_start_session_called = false;
+	void did_start_session() override
+	{
+		did_start_session_called = true;
+		SessionControllerDelegate::did_start_session();
+	}
+
+	std::optional<SessionProperty> updated_session_property_value;
+	void will_update_session_property(const SessionProperty* p) override
+	{
+		updated_session_property_value = p->copy();
 	}
 };
 
@@ -46,6 +103,12 @@ TEST_CASE("Basic Start Up of Session")
 		info->set_api_version("1.0");
 
 		bridge.process_message(std::move(message));
+
+		// Test Delegate Calls
+		REQUIRE(delegate->will_start_session_called);
+		REQUIRE(delegate->did_start_session_called);
+		REQUIRE(delegate->did_set_motion_mode_value == MotionMode::Idle);
+		REQUIRE(delegate->get_available_cameras_called);
 
 		REQUIRE(dispatcher->messages.size() == 3);
 
@@ -89,17 +152,83 @@ TEST_CASE("Basic Start Up of Session")
 
 		bridge.process_message(std::move(message));
 
-		REQUIRE(dispatcher->messages.size() == 1);
+		// Check Delegate Calls
+		REQUIRE(delegate->set_active_camera_value);
+		REQUIRE(delegate->set_active_camera_value.value().name == "cam1");
 
 		// Receive Acknowledge -----------------------------
+		REQUIRE(dispatcher->messages.size() == 1);
 		auto response = dispatcher->messages.front();
 		REQUIRE(response.has_acknowledge());
 		dispatcher->messages.pop(); // pop
+
+		REQUIRE(dispatcher->messages.size() == 0); // Ensure Empty Queue
 	}
 
-	// TODO Update Motion Mode
-	// TODO Update XForm
-	// TODO Turn Off Mode
-}
+	// Send Motion Mode Update
+	{
+		auto message = net_make_message();
+		auto payload = message.mutable_session_property();
+		payload->set_name(GlobalProperties::MotionCaptureMode().name());
+		payload->set_int_value(MotionMode::Live);
 
-// TODO Check Error Handling.
+		bridge.process_message(std::move(message));
+
+		// Check Delegate
+		REQUIRE(delegate->did_set_motion_mode_value == MotionMode::Live);
+
+		// Expect Acknowledgement
+		REQUIRE(dispatcher->messages.size() == 1);
+		auto response = dispatcher->messages.front();
+		REQUIRE(response.has_acknowledge());
+		dispatcher->messages.pop(); // pop
+
+		REQUIRE(dispatcher->messages.size() == 0); // Ensure Empty Queue
+	}
+
+	// Send Motion XForm
+	{
+		auto message = net_make_message();
+		auto payload = message.mutable_input_device_xform();
+		auto t = payload->mutable_translation();
+		auto o = payload->mutable_orientation();
+		t->set_x(1.0f);
+		t->set_y(2.0f);
+		t->set_z(3.0f);
+		o->set_x(4.0f);
+		o->set_y(5.0f);
+		o->set_z(6.0f);
+
+		bridge.process_message(std::move(message));
+
+		auto xform = delegate->received_motion_xform.value();
+		REQUIRE(xform.translation.x == 1.0f);
+		REQUIRE(xform.translation.y == 2.0f);
+		REQUIRE(xform.translation.z == 3.0f);
+		REQUIRE(xform.orientation.x == 4.0f);
+		REQUIRE(xform.orientation.y == 5.0f);
+		REQUIRE(xform.orientation.z == 6.0f);
+
+		REQUIRE(dispatcher->messages.size() == 0);
+	}
+
+	// Send Motion Mode Update
+	{
+		auto message = net_make_message();
+		auto payload = message.mutable_session_property();
+		payload->set_name(GlobalProperties::MotionCaptureMode().name());
+		payload->set_int_value(MotionMode::Idle);
+
+		bridge.process_message(std::move(message));
+
+		// Check Delegate
+		REQUIRE(delegate->did_set_motion_mode_value == MotionMode::Idle);
+
+		REQUIRE(dispatcher->messages.size() == 1);
+		auto response = dispatcher->messages.front();
+		REQUIRE(response.has_acknowledge());
+		dispatcher->messages.pop(); // pop
+
+		REQUIRE(dispatcher->messages.size() == 0); // Ensure Empty Queue
+	}
+}
