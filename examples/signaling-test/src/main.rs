8b384d7490b0d88107ac9bb0c84ce33ee051f352
Andrew Paxson <13216842+paxsonsa@users.noreply.github.com>
2023-11-29 17:28:37 -0800

    add examples

---
+++ examples/signaling-test/src/main.rs
@@ -0,0 +1,21 @@
+use tokio;
+use warp::{self, Filter};
+
+#[tokio::main]
+async fn main() {
+    let root = warp::path::end().map(|| "CineMotion Server");
+
+    let session = warp::path!("sessions")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and_then(handle_sessions);
+
+    let routes = root.or(session);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+}
+
+async fn handle_sessions(body: String) -> Result<impl warp::Reply, std::convert::Infallible> {
+    println!("body -> {}", body);
+    Ok(warp::http::StatusCode::OK)
+}
