7cdfe194cb544bb5c97b8760f1e0e78d5b7b4cc9
Andrew Paxson <andrew.paxson@icloud.com>
2023-12-23 15:34:25 -0800

    Remove indiemotion-api Signed-off-by: Andrew Paxson <andrew.paxson@icloud.com>

--- crates/indiemotion-api/src/prelude.rs
+++
@@ -1,6 +0,0 @@
-pub use crate::command::Command;
-pub use crate::error::{Error, Result};
-pub use crate::message::Message;
-pub use crate::models::*;
-pub use crate::name::Name;
-pub use crate::state::GlobalState;
